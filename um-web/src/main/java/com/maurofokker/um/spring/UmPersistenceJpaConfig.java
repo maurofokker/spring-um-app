ddfcf9c6f994b4cb370c704b73e5a8e0a0cf5222
Mauricio Galdames <mgaldamesc.azurian@bancochile.cl>
2017-07-26 17:13:13 -0400

    using settings.properties as property file to connect to mysql db

--- um-web/src/main/java/com/maurofokker/um/spring/UmPersistenceJpaConfig.java
+++ um-web/src/main/java/com/maurofokker/um/spring/UmPersistenceJpaConfig.java
@@ -20,7 +20,7 @@ import java.util.Properties;
 @Configuration
 @EnableTransactionManagement
 @ComponentScan({ "com.maurofokker.um.persistence" })
-@PropertySource({ "classpath:persistence-${persistenceTarget:h2}.properties" })
+//@PropertySource({ "classpath:persistence-${persistenceTarget:h2}.properties" })
 @EnableJpaRepositories(basePackages = "com.maurofokker.um.persistence.dao")
 public class UmPersistenceJpaConfig {
 
