974d8442319788e9af18063cbc83e5eafec836e9
Mauricio Galdames <mgaldamesc.azurian@bancochile.cl>
2017-08-07 15:17:32 -0400

    first live api test, app should be running before test

--- um-web/src/test/java/com/maurofokker/um/web/role/RoleBasicLiveTest.java
+++ um-web/src/test/java/com/maurofokker/um/web/role/RoleBasicLiveTest.java
@@ -1,11 +1,18 @@
 package com.maurofokker.um.web.role;
 
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.response.Response;
+import com.jayway.restassured.specification.RequestSpecification;
 import com.maurofokker.common.spring.util.Profiles;
 import com.maurofokker.um.spring.CommonTestConfig;
 import com.maurofokker.um.spring.UmClientConfig;
 import com.maurofokker.um.spring.UmLiveTestConfig;
+import com.maurofokker.um.util.Um;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -17,6 +24,17 @@ import org.springframework.test.context.support.AnnotationConfigContextLoader;
 @ContextConfiguration(classes = { UmLiveTestConfig.class, UmClientConfig.class, CommonTestConfig.class }, loader = AnnotationConfigContextLoader.class)
 public class RoleBasicLiveTest {
 
-    private final static String JSON = MediaType.APPLICATION_JSON.toString();
+    private final static String URI = "http://localhost:8086/api/roles";
+
+    // tests
+
+    @Test
+    public void whenAllRolesAreRetrieved_then200OK() {
+        final RequestSpecification basicAuth = RestAssured.given().auth().preemptive().basic(Um.ADMIN_EMAIL, Um.ADMIN_PASS);
+        // preemptive() is used bc is not from a browser, and we dont want to get prompted for credentials and then gives basic() auth info
+        final Response response = basicAuth.accept(ContentType.JSON).get(URI);
+
+        Assert.assertThat(response.getStatusCode(), Matchers.equalTo(200));
+    }
 
 }
