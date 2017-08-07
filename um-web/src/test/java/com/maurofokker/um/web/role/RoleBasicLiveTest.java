60e3944a47a05a5fff83ac17b088fa60fb086043
Mauricio Galdames <mgaldamesc.azurian@bancochile.cl>
2017-08-07 15:32:35 -0400

    second test that extract body response and validate that exists at least 1 role with live api test, again server must be runnning

--- um-web/src/test/java/com/maurofokker/um/web/role/RoleBasicLiveTest.java
+++ um-web/src/test/java/com/maurofokker/um/web/role/RoleBasicLiveTest.java
@@ -5,10 +5,12 @@ import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.response.Response;
 import com.jayway.restassured.specification.RequestSpecification;
 import com.maurofokker.common.spring.util.Profiles;
+import com.maurofokker.um.persistence.model.Role;
 import com.maurofokker.um.spring.CommonTestConfig;
 import com.maurofokker.um.spring.UmClientConfig;
 import com.maurofokker.um.spring.UmLiveTestConfig;
 import com.maurofokker.um.util.Um;
+import org.hamcrest.CoreMatchers;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
@@ -17,6 +19,8 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import java.util.List;
 
 
 @ActiveProfiles({Profiles.CLIENT, Profiles.TEST })
@@ -35,6 +39,14 @@ public class RoleBasicLiveTest {
         final Response response = basicAuth.accept(ContentType.JSON).get(URI);
 
         Assert.assertThat(response.getStatusCode(), Matchers.equalTo(200));
+    }
+
+    @Test
+    public void whenAllRolesAreRetrieved_thenAtLeastOneRoleExists() {
+        final Response response = RestAssured.given().auth().preemptive().basic(Um.ADMIN_EMAIL, Um.ADMIN_PASS).accept(ContentType.JSON).get(URI);
+        final List<Role> roles = response.as(List.class); // extracts from the body
+
+        Assert.assertThat(roles, CoreMatchers.not(Matchers.<Role> empty()));
     }
 
 }
