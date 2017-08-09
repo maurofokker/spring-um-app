e7df016a638f4e8dbed4e63cefa5c911d812caa6
Mauricio Galdames <mgaldamesc.azurian@bancochile.cl>
2017-08-09 18:01:38 -0400

    refactor to generic test of resources

--- common/src/main/java/com/maurofokker/common/interfaces/IWithName.java
+++ common/src/main/java/com/maurofokker/common/interfaces/IWithName.java
@@ -4,4 +4,6 @@ public interface IWithName {
 
     String getName();
 
+    void setName(final String name);
+
 }
--- um-web/src/test/java/com/maurofokker/um/web/role/RoleSimpleLiveTest.java
+++ um-web/src/test/java/com/maurofokker/um/web/role/RoleSimpleLiveTest.java
@@ -23,150 +23,44 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
+import java.util.Collection;
+
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
 
 @ActiveProfiles({Profiles.CLIENT, Profiles.TEST })
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { UmLiveTestConfig.class, UmClientConfig.class, CommonTestConfig.class }, loader = AnnotationConfigContextLoader.class)
-public class RoleSimpleLiveTest {
-
-    private final static String JSON = MediaType.APPLICATION_JSON.toString();
+public final class RoleSimpleLiveTest extends GenericSimpleLiveTest<Role> {
 
     @Autowired
     private RoleSimpleApiClient api;
 
-    // find - one
-
+    /*@Override
     @Test
-    public final void whenNonExistingResourceIsRetrieved_then404IsReceived() {
-        final Response response = getApi().findOneAsResponse(IDUtil.randomPositiveLong());
-    
-        Assert.assertThat(response.getStatusCode(), CoreMatchers.is(404));
-    }
-
-    @Test
-    public final void whenResourceIsRetrievedByNonNumericId_then400IsReceived() {
-        // When
-        final Response res = getApi().read(getUri() + WebConstants.PATH_SEP + randomAlphabetic(6));
-    
-        // Then
-        Assert.assertThat(res.getStatusCode(), CoreMatchers.is(400));
-    }
-    
-    @Test
-    public final void givenResourceForIdExists_whenResourceOfThatIdIsRetrieved_then200IsRetrieved() {
-        // Given
-        final String uriForResourseCreation = getApi().createAsResponse(createNewResource()).getHeader(HttpHeaders.LOCATION);
-        
-        //when
-        final Response response = getApi().read(uriForResourseCreation);
-    
-        // Then
-        Assert.assertThat(response.getStatusCode(), CoreMatchers.is(200));
-    }
-    
-    @Test
-    public final void whenResourceIsCreated_thenResourceIsCorrectlyRetrieved() {
-        // Given, When
-        final Role newResource = createNewResource();
-        final Role createdResource = getApi().create(newResource);
-
-        // Then
-        Assert.assertEquals(createdResource, newResource);
-    }
-
-    // find all
-
-    @Test
-    public final void whenAllResourcesAreRetrieved_then200IsReceived() {
-        // When
-        final Response response = getApi().read(getUri());
-
-        // Then
-        Assert.assertThat(response.getStatusCode(), CoreMatchers.is(200));
-    }
-
-    // create
-
-    @Test
+    @Ignore("operation not supported")
     public final void whenResourceIsCreated_then201IsReceived() {
-        // When
-        final Response response = getApi().createAsResponse(createNewResource());
 
-        // Then
-        assertThat(response.getStatusCode(), is(201));
-    }
-
-    @Test
-    public final void givenResourceHasNameWithSpace_whenResourceIsCreated_then201IsReceived() {
-        final Role newResource = createNewResource();
-        newResource.setName(randomAlphabetic(4) + " " + randomAlphabetic(4));
-
-        // When
-        final Response createAsResponse = getApi().createAsResponse(newResource);
-
-        // Then
-        assertThat(createAsResponse.getStatusCode(), is(201));
-    }
-
-    @Test
-    public final void whenResourceWithUnsupportedMediaTypeIsCreated_then415IsReceived() {
-        // When
-        final Response response = getApi().givenAuthenticated().contentType("unknown").post(getUri());
-
-        // Then
-        assertThat(response.getStatusCode(), is(415));
-    }
-
-    @Test
-    public final void whenResourceIsCreatedWithNonNullId_then409IsReceived() {
-        final Role resourceWithId = createNewResource();
-        resourceWithId.setId(5l);
-
-        // When
-        final Response response = getApi().createAsResponse(resourceWithId);
-
-        // Then
-        assertThat(response.getStatusCode(), is(409));
-    }
-
-    @Test
-    public final void whenResourceIsCreated_thenResponseContainsTheLocationHeader() {
-        // When
-        final Response response = getApi().createAsResponse(createNewResource());
-
-        // Then
-        assertNotNull(response.getHeader(HttpHeaders.LOCATION));
-    }
-
-    @Test
-    public final void givenResourceExists_whenResourceWithSameAttributeIsCreated_then409IsReceived() {
-        // Given
-        final Role newEntity = createNewResource();
-        getApi().createAsResponse(newEntity);
-
-        // when
-        final Response response = getApi().createAsResponse(newEntity);
-
-        // Then
-        assertThat(response.getStatusCode(), is(409));
-    }
+    }*/
 
     // UTIL
 
-    private final String getUri() {
-        return getApi().getUri() + WebConstants.PATH_SEP;
-    }
-
-    private final RoleSimpleApiClient getApi() {
+    @Override
+    protected final RoleSimpleApiClient getApi() {
         return api;
     }
 
-    private final Role createNewResource() {
+    @Override
+    protected final Role createNewResource() {
         return new Role(randomAlphabetic(8), Sets.<Privilege> newHashSet());
     }
 
+    @Override
+    protected Collection<Privilege> getAssociations(Role resource) {
+        return resource.getPrivileges();
+    }
+
+    @Override
+    protected Privilege createNewAssociationResource() {
+        return new Privilege(randomAlphabetic(8));
+    }
 }
