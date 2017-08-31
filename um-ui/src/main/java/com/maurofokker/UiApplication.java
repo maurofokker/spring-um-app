c2b5dc1788cd0cd3960f0798ae939c6f9f6ac50f
Mauricio Galdames <mgaldamesc.azurian@bancochile.cl>
2017-08-31 17:14:43 -0300

    consum api secured with OAuth2 (password flow) from angular js front end

--- um-ui/src/main/java/com/maurofokker/UiApplication.java
+++ um-ui/src/main/java/com/maurofokker/UiApplication.java
@@ -2,6 +2,11 @@ package com.maurofokker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * Created by mgaldamesc on 04-08-2017.
@@ -10,6 +15,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UiApplication {
 
     public static void main(final String[] args) {
-        SpringApplication.run(UiApplication.class); //INFO just to serve static files
+        SpringApplication.run(UiApplication.class, args); //INFO just to serve static files
+    }
+
+    @Configuration
+    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(final HttpSecurity http) throws Exception {
+            http.httpBasic().and().authorizeRequests().antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll().anyRequest().authenticated().and().csrf().disable();
+        }
     }
 }
