d6ca25fcb2959a6ee8cc9125433f20643290215b
maurofokker <mgaldames@gmail.com>
2017-09-08 01:57:16 -0300

    add message converter for binary format representation with kryo

--- um-web/src/test/java/com/maurofokker/um/web/privilege/PrivilegeReadOnlyLogicRestLiveTest.java
+++ um-web/src/test/java/com/maurofokker/um/web/privilege/PrivilegeReadOnlyLogicRestLiveTest.java
@@ -1,11 +1,19 @@
 package com.maurofokker.um.web.privilege;
 
+import com.jayway.restassured.response.Response;
 import com.maurofokker.um.client.template.PrivilegeRestClient;
 import com.maurofokker.um.persistence.model.Privilege;
 import com.maurofokker.um.test.live.UmReadOnlyLogicRestLiveTest;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class PrivilegeReadOnlyLogicRestLiveTest extends UmReadOnlyLogicRestLiveTest<Privilege> {
+
+    private static final String APPLICATION_KRYO = "application/x-kryo";
 
     @Autowired
     private PrivilegeRestClient api;
@@ -15,6 +23,15 @@ public class PrivilegeReadOnlyLogicRestLiveTest extends UmReadOnlyLogicRestLiveT
     }
 
     // tests
+    @Test
+    public final void giveConsumingAsKryo_whenAllResourcesAreRetrieved_then200IsReceived() {
+        // When
+        final Response response = getApi().givenReadAuthenticated().accept(APPLICATION_KRYO).get(getApi().getUri());
+
+        // Then
+        assertThat(response.getStatusCode(), is(200));
+        assertThat(response.getContentType(), equalTo(APPLICATION_KRYO));
+    }
 
     // template
 
